ff4e1a30bd0c29ac922ebd8d3d7a15279042d3f2
Kartik <hellokartikk@gmail.com>
2025-05-19 08:52:12 +0530

    Initial Commit

---
+++ single-tab/single_tabApp.swift
@@ -0,0 +1,17 @@
+//
+//  single_tabApp.swift
+//  single-tab
+//
+//  Created by kartik khorwal on 5/19/25.
+//
+
+import SwiftUI
+
+@main
+struct single_tabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ single-tabTests/single_tabTests.swift
@@ -0,0 +1,17 @@
+//
+//  single_tabTests.swift
+//  single-tabTests
+//
+//  Created by kartik khorwal on 5/19/25.
+//
+
+import Testing
+@testable import single_tab
+
+struct single_tabTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
